c79e6adbf096e11fe2a15c9cd02dec86a5f7f787
Pior Bastida <pbastida@ludia.com>
2014-05-22 12:19:25 -0400

    Adapt and improve tests a bit

--- pyramid_caching/tests/unittests/test_key_versioners.py
+++ pyramid_caching/tests/unittests/test_key_versioners.py
@@ -1,6 +1,8 @@
 import unittest
 
+import mock
 from nose_parameterized import parameterized
+
 
 from pyramid_caching.ext.redis import RedisVersionWrapper
 
@@ -30,28 +32,32 @@ class TestKeyVersioner(unittest.TestCase):
         self.assertTrue(verifyObject(IKeyVersioner, key_versioner))
 
     @parameterized.expand(KEY_VERSIONERS)
-    def test_get_multi(self, name, get_key_versioner, default_value):
+    @mock.patch('pyramid_caching.ext.redis.time')
+    def test_get_multi(self, name, get_key_versioner, default_value, m_time):
+        m_time.time.return_value = 1234.123
         key_versioner = get_key_versioner()
 
         KEYS = ['FOO', 'BAR', '2000']
-        VERSIONS = ['0', '0', '0']
+        VERSIONS = [('cache', '1234'),
+                    ('FOO', '0'),
+                    ('BAR', '0'),
+                    ('2000', '0')]
 
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
-
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
 
         for _ in range(int(self.TEST_VALUE)):
             key_versioner.incr('BAR')
-        VERSIONS = ['0', str(self.TEST_VALUE), '0']
+
+        VERSIONS[2] = ('BAR', str(self.TEST_VALUE))
 
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
-
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
 
         for _ in range(42):
             key_versioner.incr('2000')
-        VERSIONS = ['0', str(self.TEST_VALUE), '42']
+
+        VERSIONS[3] = ('2000', '42')
 
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
-
         self.assertEqual(key_versioner.get_multi(KEYS), VERSIONS)
--- pyramid_caching/tests/unittests/test_versioners.py
+++ pyramid_caching/tests/unittests/test_versioners.py
@@ -7,13 +7,12 @@ from zope.interface import implementer
 from pyramid_caching.interfaces import IIdentityInspector
 from pyramid_caching.versioner import Versioner
 
-from pyramid_caching.ext.redis import RedisVersionWrapper
-
-from fakeredis import FakeStrictRedis
-
 
 class BasicModel(object):
-    pass
+    def __init__(self, id):
+        self.id = id
+    def __hash__(self):
+        return self.id
 
 
 @implementer(IIdentityInspector)
@@ -23,7 +22,7 @@ class BasicModelIdentityInspector(object):
         return '%s.%s' % (cls.__module__, cls.__name__)
 
     def _get_obj_identity(self, obj):
-        return '%s.id_%s' % (self._get_cls_identity(obj.__class__), id(obj))
+        return '%s.id_%s' % (self._get_cls_identity(obj.__class__), hash(obj))
 
     def identify(self, obj_or_cls):
         if inspect.isclass(obj_or_cls):
@@ -35,14 +34,22 @@ class BasicModelIdentityInspector(object):
 
 
 def get_basic():
-    def instantiate_model():
-        return BasicModel()
+    def instantiate_model(id=4):
+        return BasicModel(id)
 
-    key_versioner = RedisVersionWrapper(FakeStrictRedis())
+    class TestKeyVersioner(object):
+        def __init__(self):
+            self._d = dict()
+        def get_multi(self, keys):
+            return [(k, self._d.setdefault(k, 0)) for k in keys]
+        def incr(self, key):
+            self._d[key] += 1
+    key_versioner = TestKeyVersioner()
 
     id_inspector = BasicModelIdentityInspector()
+
     versioner = Versioner(key_versioner, id_inspector.identify)
-    return key_versioner, versioner, instantiate_model, BasicModel
+    return versioner, instantiate_model, BasicModel
 
 
 class TestBasic(unittest.TestCase):
@@ -52,31 +59,31 @@ class TestBasic(unittest.TestCase):
     ])
     def test_get_multi_keys(self, test_name, test_fixture_func):
         result = test_fixture_func()
-        key_versioner, versioner, model_factory, model_cls = result
+        versioner, model_factory, model_cls = result
 
-        model1 = model_factory()
-        model2 = model_factory()
-        model3 = model_factory()
+        model1 = model_factory(id=42)
+        model2 = model_factory(id=43)
+        model3 = model_factory(id=44)
 
-        result1 = versioner.get_multi_keys(
+        result = versioner.get_multi_keys(
             [model1, model2, model3, model_cls]
         )
 
-        result2 = [
-            versioner.get_key(model1),
-            versioner.get_key(model2),
-            versioner.get_key(model3),
-            versioner.get_key(model_cls),
+        expected = [
+            'pyramid_caching.tests.unittests.test_versioners.BasicModel.id_42:v=0',
+            'pyramid_caching.tests.unittests.test_versioners.BasicModel.id_43:v=0',
+            'pyramid_caching.tests.unittests.test_versioners.BasicModel.id_44:v=0',
+            'pyramid_caching.tests.unittests.test_versioners.BasicModel:v=0',
         ]
 
-        self.assertEqual(result1, result2)
+        self.assertEqual(result, expected)
 
     @parameterized.expand([
         ("basic", get_basic),
     ])
     def test_incr(self, test_name, test_fixture_func):
         result = test_fixture_func()
-        key_versioner, versioner, model_factory, model_cls = result
+        versioner, model_factory, model_cls = result
 
         model1 = model_factory()
 
